562c00bf37e2d7faf8a71696864c045f5fc05f74
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-05-14 03:27:34 +0530

    Create Prime_Numbers.cpp

---
+++ Math/Prime_Numbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n)
+{
+    // Corner cases
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+ 
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+ 
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
+
+vector<int> Solution::sieve(int A) 
+{
+    vector<int> B;
+    for(int i=2;i<=A;i++)
+    {
+        if(isPrime(i))
+        {
+            B.push_back(i);
+        }
+    }    
+    
+    return B;
+}
+
