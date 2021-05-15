69f47cae90f84a82abf93ed07401474305592a29
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-05-16 01:33:52 +0530

    Create Binary_Search_Template.cpp

---
+++ Binary Search/Binary_Search_Template.cpp
@@ -0,0 +1,29 @@
+// binary search example in C/C++
+/* here Arr is an of integer type, n is size of array 
+   and target is element to be found */
+
+int binarySearch(int *Arr, int n, int target) {
+
+	//set stating and ending index
+	int start = 0, ending = n-1;
+
+	while(start <= ending) {
+		// take mid of the list
+		int mid = (start + end) / 2;
+
+		// we found a match
+		if(Arr[mid] == target) {
+			return mid; 
+		}
+		// go on right side
+		else if(Arr[mid] < target) {
+			start = mid + 1;
+		}
+		// go on left side
+		else {
+			end = mid - 1;
+		}
+	}
+	// element is not present in list
+	return -1;
+}
