f8d19e66d1f9c6624a231373ae213bc106337cca
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-05-16 23:46:17 +0530

    Update Binary_Search_Template.cpp

--- Binary Search/Binary_Search_Template.cpp
+++ Binary Search/Binary_Search_Template.cpp
@@ -10,7 +10,7 @@ int binarySearch(vector<int> Arr,int target)
 	while(low <= high) 
 	{
 		// take mid of the list
-		int mid = (start + end) / 2;
+		int mid = (low + high) / 2;
 		cout <<"low = "<<low<<" , high = "<< high<<" , mid = "<< mid << endl;
 
 		// we found a match
@@ -19,11 +19,11 @@ int binarySearch(vector<int> Arr,int target)
 		
 		// go on right side
 		else if(A[mid] < target) 
-			start = mid + 1;
+			low = mid + 1;
 		
 		// go on left side
 		else 
-			end = mid - 1;
+			high = mid - 1;
 	
 	}
 	// element is not present in list
