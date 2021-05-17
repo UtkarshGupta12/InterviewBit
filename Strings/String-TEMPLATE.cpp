5bfc9a3394364d842c7c367345858be3a192a29a
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-05-17 22:01:10 +0530

    Update String-TEMPLATE.cpp

--- Strings/String-TEMPLATE.cpp
+++ Strings/String-TEMPLATE.cpp
@@ -3,12 +3,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printString(vector<string> &str)
+{
+    for(int i=0;i<str.size();i++)
+    {
+        cout << str[i] << "   ";
+    }
+    cout << endl;
+}
+
 //To remove specific characters from a string
 //str.erase(remove(str.begin(), str.end(), ' '), str.end());
 
 //To transform allc character to lower case
 //transform(str.begin(), str.end(), str.begin(), ::tolower);
-
 
 int main()
 {
