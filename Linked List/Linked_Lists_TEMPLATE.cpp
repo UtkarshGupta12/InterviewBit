beaaa9f7f346ca1fed2d8b98fdbe4a6565e4959a
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-05-31 00:10:28 +0530

    Update Linked_Lists_TEMPLATE.cpp

--- Linked List/Linked_Lists_TEMPLATE.cpp
+++ Linked List/Linked_Lists_TEMPLATE.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#define M 1000000007
+using namespace std;
 
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+void printList(Node* n)
+{
+    while (n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
+    }}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt     
+    freopen("input1.txt","r",stdin);
+    // for writing output to output.txt
+    freopen("output.txt","w",stdout);
+  #endif
+
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head-> data = 1;
+    head-> next = second;
+
+    second -> data = 2;
+    second -> next = third;
+
+    third -> data = 3;
+    third -> next = NULL;
+
+    printList(head);
+
+    return 0;
+    
+}
