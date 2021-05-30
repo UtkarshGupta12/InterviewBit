504f1d01b3c1b41512cbbb4465b9182d4b99bda0
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-05-30 14:12:50 +0530

    Update Palindromic-Binary-Representation.cpp

--- Bit Manipulation/Palindromic-Binary-Representation.cpp
+++ Bit Manipulation/Palindromic-Binary-Representation.cpp
@@ -5,58 +5,25 @@
 #define M 1000000007
 using namespace std;
 
-bool IsEven(int SUM, int n)
-{
-    int num;
-    int sum=0;
-
-    for(int i=0;i<n;i++)
-    {
-        num = pow(2,i);
-        sum += num;
-        if(SUM==sum)
-            return true;
-
-        sum += num;
-        if(sum==SUM)
-            return false;
-    }
-
-}
-
 int solve(int n)
 {
     if(n==1)
         return 1;
     else if(n==2)
         return 3;
-    
-    int num;
-    int sum = 0;
-    bool odd = false;
-    bool even = false;
 
-    for(int i=0;i<n;i++)
+    int ans,count=1,len=1;
+    int p;
+
+    while(count<n)
     {
-        num = pow(2,i);
-        sum += num;
-        if(n/sum==1&&n%sum<num)
-            break;
-
-        sum += num;
-        if(n/sum==1&&n%sum<num*2)
-        {
-            if(n==sum)
-                break;
-            else
-                num*=2;
-            break; 
-        }}  
-
-    cout << "Num : "<<num<<" & Sum : "<<sum<<endl;  
-    even = IsEven(sum,n);
-    cout << "Even : "<<even<<endl;
-
+        len++;
+        if(len%2==0) 
+            p=(len-2)/2;
+        else
+            p = len/2;
+        count+=pow(2,p);
+    }
 
     return INT_MAX;
 }
@@ -69,7 +36,7 @@ int main() {
     freopen("output.txt","w",stdout);
   #endif
 
-    int ques = 4;
+    int ques = 13;
     cout <<"Question  : "<<ques<<"th "<<endl<<endl;
 
     int ans ;
