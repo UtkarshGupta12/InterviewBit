2bfa72968511e0d0263c083440013af36c5768e2
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-06-02 00:21:52 +0530

    Update Linked_Lists_TEMPLATE.cpp

--- Linked List/Linked_Lists_TEMPLATE.cpp
+++ Linked List/Linked_Lists_TEMPLATE.cpp
@@ -31,6 +31,68 @@ void push(Node** head_ref, int new_data)
     /* 4. move the head to point to the new node */
     (*head_ref) = new_node;}  
 
+Node* reverse(Node* head)
+{
+    struct Node* prev = NULL;   
+    struct Node* curr = head;
+ 
+    while (curr) {
+        struct Node* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;}
+
+Node* reverseBetween(Node* head, int m, int n)
+{
+    if (m == n)
+        return head;
+ 
+    // revs and revend is start and end respectively
+    // of the portion of the linked list which
+    // need to be reversed. revs_prev is previous
+    // of starting position and revend_next is next
+    // of end of list to be reversed.
+    Node* revs = NULL, *revs_prev = NULL;
+    Node* revend = NULL, *revend_next = NULL;
+ 
+    // Find values of above pointers.
+    int i = 1;
+    Node* curr = head;
+    while (curr && i <= n) {
+        if (i < m)
+            revs_prev = curr;
+ 
+        if (i == m)
+            revs = curr;
+ 
+        if (i == n) {
+            revend = curr;
+            revend_next = curr->next;
+        }
+ 
+        curr = curr->next;
+        i++;
+    }
+    revend->next = NULL;
+ 
+    // Reverse linked list starting with
+    // revs.
+    revend = reverse(revs);
+ 
+    // If starting position was not head
+    if (revs_prev)
+        revs_prev->next = revend;
+ 
+    // If starting position was head
+    else
+        head = revend;
+ 
+    revs->next = revend_next;
+    return head;}
+
 void insertAfter(Node* prev_node, int new_data) 
 {
    
