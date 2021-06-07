cb8ad21bde7d08e90ccafe62c2bd282e1e988ffa
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-06-08 03:30:38 +0530

    Create Check-if-a-palindorme.cpp

---
+++ Hashing/Check-if-a-palindorme.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <stack>
+#include <map>
+#include <iterator>
+#define M 1000000007
+using namespace std;
+ 
+void printVec(vector<int> &v)
+{
+    int N = v.size();
+    for(int i=0;i<N;i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout <<endl;}
+
+void printMap(unordered_map<char,int> &mp)
+{
+    cout << "KEY\tELEMENT\n";
+    for (auto itr = mp.begin(); itr != mp.end(); ++itr) {
+        cout << itr->first<< '\t' << itr->second << '\n';}}
+
+int checkPalindrome(string A)
+{
+    if(A.size()==2 && A[0] == A[1]) return 1;
+    else if(A.size()==2 && A[0] != A[1]) return 0;
+    else if(A.size()==1) return 1;
+
+    unordered_map<char,int> ump;
+    int n = A.size();
+
+    for(int i=0;i<A.size();i++)
+    {
+        if(ump.find(A[i])!=ump.end())
+        {
+            int temp = ump[A[i]];
+            if((temp+i)%(n-1)==0)
+                ump.erase(A[i]);  
+            else
+            {   
+                ump.erase(A[i]);      
+                ump.insert({A[i],temp+i});
+            }
+        }
+        else
+            ump.insert({A[i],i});
+
+        printMap(ump);cout<<endl;
+    }
+
+    if(ump.size()==0 && n%2==0)
+        return 1;
+    else if(ump.size()==1 && n%2==1 && ump[A[n/2]]==(n/2))
+        return 1;
+    else if(ump.size()==2 && ump.find(A[0])!=ump.end() && ump[A[0]]%(n-1)==0)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt     
+    freopen("input1.txt","r",stdin);
+    // for writing output to output.txt
+    freopen("output.txt","w",stdout);
+  #endif
+
+    /*vector<int> A;
+    A.push_back(10);
+    A.push_back(5);
+    A.push_back(3);
+    A.push_back(4);
+    A.push_back(3);
+    A.push_back(5);
+    A.push_back(6);*/
+
+    string A = "abacaba";
+
+    cout<<"Ques : "<<A<<endl;
+    cout<<"Length : "<<A.size()<<endl<<endl;
+
+    int ans = checkPalindrome(A);
+    cout << endl <<"Answer : "<<ans<<endl;   
+
+    return 0;
+}
+/*
+    Input : 
+        A : [1 2 3 3 4 5 6]
+        B : [3 3 5]
+    Output : [3 3 5]
+    Input : 
+        A : [1 2 3 3 4 5 6]
+        B : [3 5]
+    Output : [3 5]  */
