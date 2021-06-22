a08495b099499defe8337c3a2d7b11b33f4bb120
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-06-23 02:43:34 +0530

    Create Minimum-Subset-Sum-Diff.cpp

---
+++ Dynamic Programming/Minimum-Subset-Sum-Diff.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <bits/stdc++.h>
+#include <stack>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <iterator>
+#include <string.h>
+#define M 1000000007
+using namespace std;
+
+int MinimumSubsetSumDiff(vector<int> &A)
+{
+    sort(A.begin(),A.end());
+    int n = A.size();
+    int m = accumulate(A.begin(),A.end(),0);
+    int dp[n+1][m+1];
+    dp[0][0] = 1;
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0 && j>0) dp[i][j] = 0;
+            else if(j==0) dp[i][j] = 1;
+            else
+            {
+                if(dp[i-1][j]==1) dp[i][j] = 1; 
+                else 
+                {
+                    if(dp[i-1][j-A[i-1]]==1) dp[i][j] = 1;
+                    else dp[i][j] = 0;
+                }
+            }
+        }}   
+
+    //Printing Matrix
+    for (int i = 0; i <=n; i++){
+        for (int j = 0; j <=m; j++)
+        {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;}
+    
+    int sum;
+    for(int i=0;i<=m/2;i++)
+        if(dp[n][i]==1) sum = i;
+
+    return m-2*sum;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt     
+    freopen("input1.txt","r",stdin);
+    // for writing output to output.txt
+    freopen("output.txt","w",stdout);
+  #endif
+  
+    vector<int> A = {1,5,6};
+    //vector<int> B = {1,3,4,5};
+    //int C = 1;
+    
+    cout<<"A : ";printVec(A);
+    //cout<<"Weights (B) : ";printVec(B);
+    //cout<<"Sum : "<<C<<endl<<endl;
+
+    int ans = MinimumSubsetSumDiff(A);
+    cout<<endl<<"Answer : "<<ans<<endl;
+
+    return 0;}
