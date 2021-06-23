a2912cd0c99d49c90af84fe55cc61276fc0e21d6
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-06-23 20:13:58 +0530

    Create Unbounded-Knapsack.cpp

---
+++ Dynamic Programming/Unbounded-Knapsack.cpp
@@ -0,0 +1,34 @@
+int solve(vector<int> &A,int C)
+{
+    int n = A.size();
+    int m = C;
+    int dp[n+1][m+1];
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0||j==0)
+                dp[i][j] = 0;
+            else
+            {  
+                if(j<A[i-1])
+                    dp[i][j] = dp[i-1][j];
+                else 
+                {
+                    dp[i][j] = max(dp[i-1][j],A[i-1]+dp[i][j-A[i-1]]);   
+                }
+            }    
+        }
+    }
+
+    //Printing Matrix
+    for (int i = 0; i <=n; i++){
+        for (int j = 0; j <=m; j++)
+        {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;}
+    
+    return dp[n][m];
+}
