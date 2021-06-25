1b1450cf87f0d4c4215d56dee9d5d33047434914
UtkarshGupta12 <51991010+UtkarshGupta12@users.noreply.github.com>
2021-06-25 20:35:52 +0530

    Create Largest-Palindromic-Substring.cpp

---
+++ Dynamic Programming/Largest-Palindromic-Substring.cpp
@@ -0,0 +1,33 @@
+int solve(string &A)
+{
+    int n = A.size();
+    reverse(A.begin(),A.end());
+    string B = A;
+    reverse(A.begin(),A.end());
+    int m = A.size();
+    int dp[n+1][m+1];
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0||j==0)
+                dp[i][j] = 0;
+            else
+            {  
+                if(A[i-1]==B[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }    
+        }
+    }
+
+    //Printing Matrix
+    for (int i = 0; i <=n; i++){
+        for (int j = 0; j <=m; j++)
+        {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;}
+
+    return dp[n][m];
+}
